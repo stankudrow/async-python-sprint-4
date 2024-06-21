dc75e69063dacb22daade97e5ced3476e072adfa
Stanley Kudrow <stankudrow@reply.no>
2024-06-22 01:24:51 +0300

    update the migration  file

--- migrations/versions/2024_06_16_1357-5c59b447e8f5_init_the_sprint4_db.py
+++ migrations/versions/2024_06_16_1357-5c59b447e8f5_init_the_sprint4_db.py
@@ -1,9 +1,8 @@
-"""init the sprint4 db
+"""init the sprint4 database
 
 Revision ID: 5c59b447e8f5
 Revises: 
 Create Date: 2024-06-16 13:57:49.196181
-
 """
 
 from typing import Sequence, Union
@@ -19,16 +18,33 @@ branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
-URLS_TABLE: str = "urls"
+# tables
+URIS_TABLE: str = "uris"
+
+# columns
+SHORT_URI_COL = "shorten_uri"
+
+# indexes
+IDX_SUFFIX = "idx"
+SHORT_URI_INDEX = f"{SHORT_URI_COL}_{IDX_SUFFIX}"
 
 
 def upgrade() -> None:
     op.create_table(
-        URLS_TABLE,
+        URIS_TABLE,
         sa.Column("id", sa.Integer(), primary_key=True),
-        sa.Column("url", sa.Text(), nullable=False),
+        sa.Column("uri", sa.Text(), nullable=False),
+        sa.Column(SHORT_URI_COL, sa.String, unique=True, nullable=False),
+    )
+    op.create_index(
+        index_name=SHORT_URI_INDEX, table_name=URIS_TABLE, columns=[SHORT_URI_COL]
     )
 
 
 def downgrade() -> None:
-    op.drop_table(URLS_TABLE)
+    op.drop_index(
+        index_name=SHORT_URI_INDEX,
+        table_name=URIS_TABLE,
+        if_exists=True,
+    )
+    op.drop_table(URIS_TABLE)
