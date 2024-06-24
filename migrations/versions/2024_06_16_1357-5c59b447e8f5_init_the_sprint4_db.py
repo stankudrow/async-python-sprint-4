9257db481f06bdce44ad7c27ab37327ec455b9fe
Stanley Kudrow <stankudrow@reply.no>
2024-06-24 23:35:50 +0300

    update migrations

--- migrations/versions/2024_06_16_1357-5c59b447e8f5_init_the_sprint4_db.py
+++ migrations/versions/2024_06_16_1357-5c59b447e8f5_init_the_sprint4_db.py
@@ -5,6 +5,7 @@ Revises:
 Create Date: 2024-06-16 13:57:49.196181
 """
 
+from enum import Enum
 from typing import Sequence, Union
 
 from alembic import op
@@ -19,32 +20,64 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 # tables
-URIS_TABLE: str = "uris"
+URLS_TABLE: str = "urls"
 
 # columns
-SHORT_URI_COL = "shorten_uri"
+SHORT_URL_COL = "short_url"
 
 # indexes
 IDX_SUFFIX = "idx"
-SHORT_URI_INDEX = f"{SHORT_URI_COL}_{IDX_SUFFIX}"
+SHORT_URL_INDEX = f"{SHORT_URL_COL}_{IDX_SUFFIX}"
+
+
+# data
+class UrlVisibilityTypes(str, Enum):
+    public = "public"
+    private = "private"
 
 
 def upgrade() -> None:
     op.create_table(
-        URIS_TABLE,
-        sa.Column("id", sa.Integer(), primary_key=True),
-        sa.Column("uri", sa.Text(), nullable=False),
-        sa.Column(SHORT_URI_COL, sa.String, unique=True, nullable=False),
+        URLS_TABLE,
+        sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column("url", sa.Text, nullable=False, comment="URL"),
+        sa.Column(
+            SHORT_URL_COL,
+            sa.String,
+            unique=True,
+            nullable=False,
+            comment="Shortened URL",
+        ),
+        sa.Column(
+            "is_gone",
+            sa.Boolean,
+            default=False,
+            server_default="False",
+            nullable=False,
+            comment="The mark for deletion",
+        ),
+        sa.Column(
+            "visibility",
+            sa.Enum(UrlVisibilityTypes),
+            default=UrlVisibilityTypes.public,
+            server_default=UrlVisibilityTypes.public,
+            nullable=False,
+            comment="The kind of URL by visibility: public, private, etc.",
+        ),
     )
     op.create_index(
-        index_name=SHORT_URI_INDEX, table_name=URIS_TABLE, columns=[SHORT_URI_COL]
+        index_name=SHORT_URL_INDEX,
+        table_name=URLS_TABLE,
+        columns=[SHORT_URL_COL],
+        unique=True,
+        if_not_exists=True,
     )
 
 
 def downgrade() -> None:
     op.drop_index(
-        index_name=SHORT_URI_INDEX,
-        table_name=URIS_TABLE,
+        index_name=SHORT_URL_INDEX,
+        table_name=URLS_TABLE,
         if_exists=True,
     )
-    op.drop_table(URIS_TABLE)
+    op.drop_table(URLS_TABLE)
