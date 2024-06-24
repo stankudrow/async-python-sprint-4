edaac4979084e3c6a282af25053674deb7bf8add
Stanley Kudrow <stankudrow@reply.no>
2024-06-24 23:30:41 +0300

    update models

---
+++ sprint4/models/__init__.py
@@ -0,0 +1,1 @@
+from sprint4.models.internal import ServiceStatistics
--- sprint4/models/urls.py
+++ sprint4/models/urls.py
@@ -1,14 +1,66 @@
-from pydantic import AnyHttpUrl, BaseModel, field_serializer
+from enum import Enum
+
+from pydantic import AnyHttpUrl, BaseModel, ConfigDict, NonNegativeInt, field_serializer
+
+from sprint4.core.url_shorteners import ShortenersEnum
 
 
-class HttpUrl(BaseModel):
+class UrlVisibilityTypes(str, Enum):
+    public = "public"
+    private = "private"
+
+
+class HttpUrlBase(BaseModel):
     url: AnyHttpUrl
-    short_url: AnyHttpUrl
+    visibility: UrlVisibilityTypes = UrlVisibilityTypes.public
 
     @field_serializer("url")
     def serialize_url(self, url: AnyHttpUrl, _info) -> str:
         return str(url)
 
+    @field_serializer("visibility")
+    def serialize_visibility(self, visibility: UrlVisibilityTypes, _info) -> str:
+        return visibility.value
+
+
+class HttpUrlModel(HttpUrlBase):
+    short_url: AnyHttpUrl
+
     @field_serializer("short_url")
     def serialize_short_url(self, short_url: AnyHttpUrl, _info) -> str:
         return str(short_url)
+
+
+class HttpUrlRow(HttpUrlModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: NonNegativeInt
+    is_gone: bool = False
+
+
+class HttpUrlFilter(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: None | NonNegativeInt = None
+    url: None | AnyHttpUrl = None
+    short_url: None | AnyHttpUrl = None
+    is_gone: None | bool = None
+    visibility: None | UrlVisibilityTypes = None
+
+    @field_serializer("url")
+    def serialize_url(self, url: AnyHttpUrl, _info) -> None | str:
+        return str(url) if url else url
+
+    @field_serializer("short_url")
+    def serialize_short_url(self, short_url: AnyHttpUrl, _info) -> None | str:
+        return str(short_url) if short_url else short_url
+
+    @field_serializer("visibility")
+    def serialize_visibility(
+        self, visibility: None | UrlVisibilityTypes, _info
+    ) -> None | str:
+        return visibility.value if visibility else visibility
+
+
+class HttpUrlAddRequest(HttpUrlBase):
+    shortener_code: ShortenersEnum = ShortenersEnum.clckru
